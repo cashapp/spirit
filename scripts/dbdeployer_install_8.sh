fd4a697ea21fc8786c5459ec2ead2813bc6c25bc
Morgan Tocker <mtocker@squareup.com>
2023-02-03 09:12:09 -0700

    Upgrade MySQL version

--- scripts/dbdeployer_install_8.sh
+++ scripts/dbdeployer_install_8.sh
@@ -6,7 +6,4 @@ sudo apt install -y libncurses5
 go install github.com/datacharmer/dbdeployer@latest
 
 dbdeployer init
-
-dbdeployer downloads get mysql-8.0.30-linux-glibc2.17-x86_64-minimal.tar.xz
-dbdeployer unpack mysql-8.0.30-linux-glibc2.17-x86_64-minimal.tar.xz
-dbdeployer deploy single 8.0.30
+dbdeployer deploy single 8.0.32
