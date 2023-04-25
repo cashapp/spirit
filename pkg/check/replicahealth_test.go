a90d43c77c1f96c38137de2fb128f94337660bb5
Morgan Tocker <mtocker@squareup.com>
2023-04-25 14:14:19 -0600

    increase test coverage

---
+++ pkg/check/replicahealth_test.go
@@ -0,0 +1,47 @@
+package check
+
+import (
+	"context"
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "github.com/pingcap/tidb/parser/test_driver"
+
+	"github.com/sirupsen/logrus"
+	"github.com/squareup/spirit/pkg/table"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReplicaHealth(t *testing.T) {
+	r := Resources{
+		Table: &table.TableInfo{TableName: "test"},
+		Alter: "RENAME TO newtablename",
+	}
+	err := replicaHealth(context.Background(), r, logrus.New())
+	assert.NoError(t, err) // if no replica, it returns no error.
+
+	// use a non-replica. this will return an error.
+	r.Replica, err = sql.Open("mysql", dsn())
+	assert.NoError(t, err)
+	err = replicaHealth(context.Background(), r, logrus.New())
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, "replica is not healthy")
+
+	// use an actual replica
+	replicaDSN := os.Getenv("REPLICA_DSN")
+	if replicaDSN == "" {
+		t.Skip("skipping test because REPLICA_DSN not set")
+	}
+	r.Replica, err = sql.Open("mysql", replicaDSN)
+	assert.NoError(t, err)
+	err = replicaHealth(context.Background(), r, logrus.New())
+	assert.NoError(t, err) // all looks good of course.
+
+	// use a completely invalid DSN.
+	// golang sql.Open lazy loads, so this is possible.
+	r.Replica, err = sql.Open("mysql", "msandbox:msandbox@tcp(127.0.0.1:22)/test")
+	assert.NoError(t, err)
+	err = replicaHealth(context.Background(), r, logrus.New())
+	assert.Error(t, err) // invalid
+}
---
+++ pkg/check/replicaprivileges_test.go
@@ -0,0 +1,33 @@
+package check
+
+import (
+	"context"
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "github.com/pingcap/tidb/parser/test_driver"
+
+	"github.com/sirupsen/logrus"
+	"github.com/squareup/spirit/pkg/table"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReplicaPrivileges(t *testing.T) {
+	// use an actual replica
+	replicaDSN := os.Getenv("REPLICA_DSN")
+	if replicaDSN == "" {
+		t.Skip("skipping test because REPLICA_DSN not set")
+	}
+	r := Resources{
+		Table: &table.TableInfo{TableName: "test"},
+		Alter: "RENAME TO newtablename",
+	}
+	err := replicaPrivilegeCheck(context.Background(), r, logrus.New())
+	assert.NoError(t, err) // if no replica, it returns no error.
+
+	r.Replica, err = sql.Open("mysql", replicaDSN)
+	assert.NoError(t, err) // no error
+	err = replicaPrivilegeCheck(context.Background(), r, logrus.New())
+	assert.NoError(t, err) // user has privileges
+}
