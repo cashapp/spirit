84c86be57adb45c499b2184a64d7e280541fb0e3
Joy Nag <jnag@squareup.com>
2023-06-26 11:05:53 +1000

    Update binary name

--- scripts/dbdeployer_install_8026.sh
+++ scripts/dbdeployer_install_8026.sh
@@ -7,9 +7,8 @@ go install github.com/datacharmer/dbdeployer@latest
 
 dbdeployer init
 
-dbdeployer downloads list
-dbdeployer downloads get mysql-8.0.26-linux-glibc2.17-x86_64-minimal.tar.xz
-dbdeployer unpack mysql-8.0.26-linux-glibc2.17-x86_64-minimal.tar.xz
+dbdeployer downloads get mysql-8.0.26-linux-glibc2.17-x86_64-minimal-rebuild.tar.xz
+dbdeployer unpack mysql-8.0.26-linux-glibc2.17-x86_64-minimal-rebuild.tar.xz
 
 dbdeployer deploy single 8.0.26
 
