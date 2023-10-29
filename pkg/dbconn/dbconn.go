1f8e478a819675b3669df8a7a8e49eadd1e182cd
Morgan Tocker <mtocker@squareup.com>
2023-10-29 10:02:39 -0600

    Refactor RetryableTrx for readability

--- pkg/dbconn/dbconn.go
+++ pkg/dbconn/dbconn.go
@@ -8,7 +8,6 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/cashapp/spirit/pkg/utils"
 	"github.com/go-sql-driver/mysql"
 )
 
@@ -60,90 +59,108 @@ func canRetryError(err error) bool {
 // RetryableTransaction retries all statements in a transaction, retrying if a statement
 // errors, or there is a deadlock. It will retry up to maxRetries times.
 func RetryableTransaction(ctx context.Context, db *sql.DB, ignoreDupKeyWarnings bool, config *DBConfig, stmts ...string) (int64, error) {
-	var err error
-	var trx *sql.Tx
-	var rowsAffected int64
-RETRYLOOP:
+	var (
+		err          error
+		trx          *sql.Tx
+		rowsAffected int64
+		isFatal      bool
+	)
 	for i := 0; i < config.MaxRetries; i++ {
-		// Start a transaction
-		if trx, err = db.BeginTx(ctx, nil); err != nil {
-			backoff(i)
-			continue RETRYLOOP // retry
-		}
-		// Execute all statements.
-		for _, stmt := range stmts {
-			if stmt == "" {
-				continue
+		func() {
+			// Start a transaction
+			if trx, err = db.BeginTx(ctx, nil); err != nil {
+				return
 			}
-			var res sql.Result
-			if res, err = trx.ExecContext(ctx, stmt); err != nil {
-				if canRetryError(err) {
-					utils.ErrInErr(trx.Rollback()) // Rollback
-					backoff(i)
-					continue RETRYLOOP // retry
-				}
-				utils.ErrInErr(trx.Rollback()) // Rollback
-				return rowsAffected, err
-			}
-			// Even though there was no ERROR we still need to inspect SHOW WARNINGS
-			// This is because many of the statements use INSERT IGNORE.
-			warningRes, err := trx.QueryContext(ctx, "SHOW WARNINGS") //nolint: execinquery
-			if err != nil {
-				utils.ErrInErr(trx.Rollback()) // Rollback
-				return rowsAffected, err
-			}
-			defer warningRes.Close()
-			var level, code, message string
-			for warningRes.Next() {
-				err = warningRes.Scan(&level, &code, &message)
+			// If anything was non successful as we exit
+			// then rollback before either retrying or finishing up
+			// If we are going to retry, then backoff first.
+			defer func() {
 				if err != nil {
-					utils.ErrInErr(trx.Rollback()) // Rollback
-					return rowsAffected, err
+					_ = trx.Rollback()
+					if i < config.MaxRetries-1 && !isFatal {
+						backoff(i)
+					}
 				}
-				// We won't receive out of range warnings (1264)
-				// because the SQL mode has been unset. This is important
-				// because a historical value like 0000-00-00 00:00:00
-				// might exist in the table and needs to be copied.
-				if code == "1062" && ignoreDupKeyWarnings {
-					continue // ignore duplicate key warnings
-				} else if code == "3170" {
-					// ER_CAPACITY_EXCEEDED
-					// "Memory capacity of 8388608 bytes for 'range_optimizer_max_mem_size' exceeded.
-					// Range optimization was not done for this query."
-					// i.e. the query still executes it just doesn't optimize perfectly
+			}()
+			// Execute all statements.
+			for _, stmt := range stmts {
+				if stmt == "" {
 					continue
-				} else {
-					utils.ErrInErr(trx.Rollback())
-					return rowsAffected, fmt.Errorf("unsafe warning migrating chunk: %s, query: %s", message, stmt)
 				}
+				var res sql.Result
+				if res, err = trx.ExecContext(ctx, stmt); err != nil {
+					if !canRetryError(err) {
+						isFatal = true
+					}
+					return
+				}
+				// Even though there was no ERROR we still need to inspect SHOW WARNINGS
+				// This is because many of the statements use INSERT IGNORE.
+				var warningRes *sql.Rows
+				warningRes, err = trx.QueryContext(ctx, "SHOW WARNINGS") //nolint: execinquery
+				if err != nil {
+					return
+				}
+				defer warningRes.Close()
+				var level, code, message string
+				for warningRes.Next() {
+					err = warningRes.Scan(&level, &code, &message)
+					if err != nil {
+						return
+					}
+					// We won't receive out of range warnings (1264)
+					// because the SQL mode has been unset. This is important
+					// because a historical value like 0000-00-00 00:00:00
+					// might exist in the table and needs to be copied.
+					if code == "1062" && ignoreDupKeyWarnings {
+						continue // ignore duplicate key warnings
+					} else if code == "3170" {
+						// ER_CAPACITY_EXCEEDED
+						// "Memory capacity of 8388608 bytes for 'range_optimizer_max_mem_size' exceeded.
+						// Range optimization was not done for this query."
+						// i.e. the query still executes it just doesn't optimize perfectly
+						continue
+					} else {
+						isFatal = true
+						err = fmt.Errorf("unsafe warning migrating chunk: %s, query: %s", message, stmt)
+						return
+					}
+				}
+				if warningRes.Err() != nil {
+					err = warningRes.Err()
+					return
+				}
+				// As long as it is a statement that supports affected rows (err == nil)
+				// Get the number of rows affected and add it to the total balance.
+				// This uses errC because some statements don't support affected rows,
+				// and that's absolutely fine!
+				count, errC := res.RowsAffected()
+				if errC == nil { // affectedRows is supported
+					rowsAffected += count
+				}
+			} // end for each statement
+			// Commit it!
+			if err = trx.Commit(); err != nil {
+				return
 			}
-			if warningRes.Err() != nil {
-				utils.ErrInErr(trx.Rollback())
-				return rowsAffected, warningRes.Err()
-			}
-			// As long as it is a statement that supports affected rows (err == nil)
-			// Get the number of rows affected and add it to the total balance.
-			count, err := res.RowsAffected()
-			if err == nil { // supported
-				rowsAffected += count
-			}
+		}()
+		if isFatal { // don't retry loop if fatal
+			return rowsAffected, err
 		}
-		if err != nil {
-			utils.ErrInErr(trx.Rollback()) // Rollback
-			backoff(i)
-			continue RETRYLOOP
+		// If error is nil, break the loop
+		// The transaction was successful
+		if err == nil {
+			break // success, time
 		}
-		// Commit it.
-		if err = trx.Commit(); err != nil {
-			utils.ErrInErr(trx.Rollback())
-			backoff(i)
-			continue RETRYLOOP
-		}
-		// Success!
-		return rowsAffected, nil
+	} // end of retry loop
+
+	// it is possible the error is non-fatal but the maximum
+	// number of retries has been attempted. In which case err
+	// will be non-nil.
+	if err != nil {
+		return rowsAffected, err
 	}
-	// We failed too many times, return the last error
-	return rowsAffected, err
+	return rowsAffected, nil
 }
 
 // backoff sleeps a few milliseconds before retrying.
