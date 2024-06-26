ae70bf664824d0ce3e58f7a0c742ffa44c70d1d5
Jay Janssen <jayj@squareup.com>
2024-06-26 10:54:26 -0400

    create dbconn.MetadataLock struct and tests

---
+++ pkg/dbconn/metadatalock_test.go
@@ -0,0 +1,80 @@
+package dbconn
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/cashapp/spirit/pkg/testutils"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetadataLock(t *testing.T) {
+	lockName := "test"
+	logger := logrus.New()
+	mdl, err := NewMetadataLock(context.Background(), testutils.DSN(), lockName, logger)
+	assert.NoError(t, err)
+	assert.NotNil(t, mdl)
+
+	// Confirm a second lock cannot be acquired
+	_, err = NewMetadataLock(context.Background(), testutils.DSN(), lockName, logger)
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, "lock is held by another connection")
+
+	// Close the original mdl
+	err = mdl.Close()
+	assert.NoError(t, err)
+
+	// Confirm a new lock can be acquired
+	mdl3, err := NewMetadataLock(context.Background(), testutils.DSN(), lockName, logger)
+	assert.NoError(t, err)
+	err = mdl3.Close()
+	assert.NoError(t, err)
+}
+
+func TestMetadataLockContextCancel(t *testing.T) {
+	lockName := "test-cancel"
+
+	logger := logrus.New()
+	ctx, cancel := context.WithCancel(context.Background())
+	mdl, err := NewMetadataLock(ctx, testutils.DSN(), lockName, logger)
+	assert.NoError(t, err)
+	assert.NotNil(t, mdl)
+
+	// Cancel the context
+	cancel()
+
+	// Wait for the lock to be released
+	<-mdl.closeCh
+
+	// Confirm the lock is released by acquiring a new one
+	mdl2, err := NewMetadataLock(context.Background(), testutils.DSN(), lockName, logger)
+	assert.NoError(t, err)
+	assert.NotNil(t, mdl2)
+	err = mdl2.Close()
+	assert.NoError(t, err)
+}
+
+func TestMetadataLockRefresh(t *testing.T) {
+	lockName := "test-refresh"
+	logger := logrus.New()
+	mdl, err := NewMetadataLock(context.Background(), testutils.DSN(), lockName, logger)
+	assert.NoError(t, err)
+	assert.NotNil(t, mdl)
+
+	// override the refresh interval to 5 seconds
+	mdl.refreshInterval = 2 * time.Second
+
+	// wait for the refresh to happen
+	time.Sleep(5 * time.Second)
+
+	// Confirm the lock is still held
+	_, err = NewMetadataLock(context.Background(), testutils.DSN(), lockName, logger)
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, "lock is held by another connection")
+
+	// Close the lock
+	err = mdl.Close()
+	assert.NoError(t, err)
+}
