5d54b53a03592c347ce795b5c5cb0e187497e953
Jay Janssen <jayj@squareup.com>
2024-06-28 16:06:35 -0400

    linter fixes

--- pkg/row/copier_eta_history_test.go
+++ pkg/row/copier_eta_history_test.go
@@ -13,12 +13,12 @@ func TestCopierETAHistory(t *testing.T) {
 
 	// Add an ETA
 	history.addCurrentEstimateAndCompare(1 * time.Hour)
-	assert.Equal(t, 1, len(history.etaHistory))
+	assert.Len(t, history.etaHistory, 1)
 	assert.Equal(t, "", history.getComparison())
 
 	// Add another ETA and confirm it is NOT stored because it is too recent
 	history.addCurrentEstimateAndCompare(55 * time.Minute)
-	assert.Equal(t, 1, len(history.etaHistory))
+	assert.Len(t, history.etaHistory, 1)
 
 	// Even though the ETA was not stored, the latest estimate should still be updated
 	assert.Equal(t, "5m from 0s ago", history.getComparison())
@@ -28,7 +28,7 @@ func TestCopierETAHistory(t *testing.T) {
 	history.addETA(copierETA{estimate: 3 * time.Hour, asOf: time.Now().Add(-2 * time.Hour)})
 	history.addETA(copierETA{estimate: 2 * time.Hour, asOf: time.Now().Add(-1 * time.Hour)})
 	history.addETA(copierETA{estimate: 1 * time.Hour, asOf: time.Now()})
-	assert.Equal(t, 3, len(history.etaHistory))
+	assert.Len(t, history.etaHistory, 3)
 	assert.Equal(t, "±0m from 2h ago", history.getComparison())
 
 	// Create a new CopierETAHistory with a history of 24 ETAs
@@ -40,10 +40,10 @@ func TestCopierETAHistory(t *testing.T) {
 		})
 	}
 	// Oldest history was auto-removed
-	assert.Equal(t, 23, len(history.etaHistory))
+	assert.Len(t, history.etaHistory, 23)
 	assert.Equal(t, "±0m from 22h ago", history.getComparison())
 
 	comparison := history.addCurrentEstimateAndCompare(30 * time.Minute)
-	assert.Equal(t, 24, len(history.etaHistory))
+	assert.Len(t, history.etaHistory, 24)
 	assert.Equal(t, "-30m from 23h ago", comparison)
 }
